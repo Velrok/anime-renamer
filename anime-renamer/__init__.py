374f6e074d3598e9eff64931ad08fbef74f5e4c9
Velrok <waldemar.schwan@gmail.com>
2013-07-25 18:58:25 +0200

    argument parsing and a little validationn

--- anime-renamer/__init__.py
+++ anime-renamer/__init__.py
@@ -7,15 +7,28 @@ it will move the file to the given folder using the name schema:
 <anime-name>_E<episode-number>.<extension>
 
 Usage:
-  anime-renamer <input-file> [dest-folder]
+  anime-renamer <input_file> [<dest_folder>]
 
 
 """
 
 from docopt import docopt
+import os.path
+
+class RuntimeError(Exception):
+  pass
+
+
+def rename(filename, dest_folder):
+  if not os.path.exists(filename):
+    raise RuntimeError("file {} does not exist".format(filename))
+  if dest_folder and (not os.path.exists(dest_folder)):
+    raise RuntimeError("destination folder {} does not exist".format(dest_folder))
 
 
 if __name__ == "__main__":
   args = docopt(__doc__)
-  print args
-  print "hello wordl"
+  try:
+    rename(args['<input_file>'], args['<dest_folder>'])
+  except RuntimeError as e:
+    print "RuntimeError:", e
